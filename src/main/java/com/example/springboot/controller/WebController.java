c0723e77399d6f7dfc5f3d6284b518814081804e
wangxiaolong <wxl991020@126.com>
2026-03-04 22:21:31 +0800

    init

--- src/main/java/com/example/springboot/controller/WebController.java
+++ src/main/java/com/example/springboot/controller/WebController.java
@@ -1,6 +1,8 @@
 package com.example.springboot.controller;
 
 import com.example.springboot.common.Result;
+import com.example.springboot.service.WebService;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +14,9 @@ import java.util.Map;
 @RestController
 public class WebController {
 
+    @Resource
+    WebService webService;
+
     @GetMapping(value = "hello")
     public  Result hello() {
         return Result.success();
@@ -19,15 +24,8 @@ public class WebController {
 
     @GetMapping(value = "hello2")
     public  Result hello2() {
-        Map<Object,Object> map = new HashMap<>();
-        List<Object> list = new ArrayList<>();
-        for(int i = 0; list.size() < 10; i++){
-            list.add("hello" + i);
-        }
-        map.put("hello2",list);
-        map.put("hello","world");
+        Map<Object,Object> map = webService.hello2();
         return Result.success(map);
-//        return map;
     }
 
 }
--- src/main/java/com/example/springboot/service/WebService.java
+++ src/main/java/com/example/springboot/service/WebService.java
@@ -2,7 +2,24 @@ package com.example.springboot.service;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @Service
 public class WebService {
 
+
+    public Map<Object, Object> hello2() {
+
+        Map<Object,Object> map = new HashMap<>();
+        List<Object> list = new ArrayList<>();
+        for(int i = 0; list.size() < 10; i++){
+            list.add("hello" + i);
+        }
+        map.put("hello2",list);
+        map.put("hello","world");
+        return map;
+    }
 }
